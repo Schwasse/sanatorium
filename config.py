b1d5a662637ea0c7464c6a08d3fa989f1b4390b2
Schwasse <146382439+Schwasse@users.noreply.github.com>
2025-05-26 22:31:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(basedir, 'static', 'uploads')
 class Config:
-    SECRET_KEY = 'секретный_ключ'  # используй os.urandom или .env файл
+    SECRET_KEY = 'секретный_ключ'  
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Rotshield7@localhost:5433/sanatory'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOGGING_CONFIG = {
